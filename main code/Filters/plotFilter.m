7914e1aaabc102b7766f41d75a509a07a30b9515
youefkh05 <youefkh05@gmail.com>
2024-11-28 10:39:14 +0200

    Added plotting with titles
    Now you can plot easily

---
+++ main code/Filters/plotFilter.m
@@ -0,0 +1,21 @@
+function plotFilter(Filter, Fs, titleStr)
+    % Function to open fvtool and add a title annotation at the bottom.
+    % Inputs:
+    %   Filter - The filter object (e.g., bandpass filter)
+    %   Fs - Sampling frequency (in Hz)
+    %   titleStr - Title to be displayed below the filter plot
+
+    % Open fvtool without title
+    h = fvtool(Filter, 'Fs', Fs);
+
+    % Pause to allow fvtool to render the figure
+    pause(0.5);  % Adjust if needed based on your system's speed
+
+    % Get the position of the figure to place the annotation
+    figPos = get(h, 'Position');  % Get the figure's position [left, bottom, width, height]
+
+    % Add an annotation (title) at the bottom of the figure
+    annotation('textbox', [0.35, 0.01, 0.3, 0.05], 'String', titleStr, ...
+        'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle', ...
+        'FontSize', 12, 'FontWeight', 'bold', 'EdgeColor', 'none');
+end
